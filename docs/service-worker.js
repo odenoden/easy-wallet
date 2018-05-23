3e42ee862b0c90ab34394a92831172e7473e6257
odenoden <fs209ueda@gmail.com>
2018-05-23 22:20:58 +0900

    github page公開

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f7ad6455372ee3915be058a1a0fb34e2.js"
+  "./precache-manifest.500f72cb8ac8993bcb5b392dccacdc22.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "easy-wallet"});
@@ -27,6 +27,19 @@ workbox.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "index.html",
+    "revision": "d9889f252f5b7fee3770975f0fb2ee71"
+  },
+  {
+    "url": "precache-manifest.f7ad6455372ee3915be058a1a0fb34e2.js",
+    "revision": "f7ad6455372ee3915be058a1a0fb34e2"
+  },
+  {
+    "url": "service-worker.js",
+    "revision": "29e0cefacfd2cf52e25a596d35f62ac7"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
